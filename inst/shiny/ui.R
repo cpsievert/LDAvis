7ddc8178935a45187f8441e4ab0255bbc9430fc8
Carson Sievert <cpsievert1@gmail.com>
2014-03-21 15:20:47 -0500

    Remove topic input box and reorder ui controls

--- inst/shiny/ui.R
+++ inst/shiny/ui.R
@@ -3,9 +3,6 @@ library(LDAvis)
 #anytime a file under the assets folder is changed, LDAvis must be reinstalled (to reflect the change)!
 addResourcePath('assets', system.file('shiny', 'assets', package='LDAvis'))
 
-topic.choices <- paste(0L:length(topic.proportion))
-names(topic.choices) <-  c("None Selected", topic.choices[-1])
-
 # Thanks Winston! https://github.com/wch/testapp/blob/master/custom-style/ui.R
 widget_style <-
   "display: inline-block;
@@ -33,28 +30,25 @@ shinyUI(bootstrapPage(
   ),
 
   wellPanel(
+    div(style = widget_style,
+        selectInput("distance", "Topical Distance Calculation", choices = c("Jensen-Shannon" = "JS", 
+                                                                            "Symmetric Kullback-Leibler" = "KL"))
+    ),
+    div(style = widget_style,
+        selectInput("scaling", "Multidimensional Scaling Method", choices = c("Classical (PCA)" = "PCA", 
+                                                                              "Kruskal's Non-Metric" = "kruskal",
+                                                                              "Sammon's Non-Linear Mapping" = "sammon"))
+    ),
     div(style = widget_style, sliderInput("kmeans", "Number of clusters", min=1, max=10, value=1)),
     div(style = widget_style,
         sliderInput("nTerms", "Number of terms", min=1, max=50, value=30)
     ), 
     div(style = widget_style,
         sliderInput("lambda", "Value of lambda", min=0, max=1, value=0.6)
-    ),
-    div(style = widget_style,
-        selectInput("distance", "Distance Calculation", choices = c("Jensen-Shannon" = "JS", 
-                                                                    "Symmetric Kullback-Leibler" = "KL"))
-    ),
-    div(style = widget_style,
-        selectInput("scaling", "Multidimensional Scaling Method", choices = c("Classical (PCA)" = "PCA", 
-                                                                    "Kruskal's Non-Metric" = "kruskal",
-                                                                    "Sammon's Non-Linear Mapping" = "sammon"))
-    ),
-    div(style = widget_style,
-        selectInput("currentTopic", "Select a Topic", choices = topic.choices)
     )
   ),
-
-  mainPanel(#the el parameter in the js code selects the outputIds
-             HTML(paste0("<div id=\"mdsDat\" class=\"shiny-scatter-output\"><svg /></div>"))
+  
+  #the el parameter in the js code selects the outputIds
+  mainPanel(HTML(paste0("<div id=\"mdsDat\" class=\"shiny-scatter-output\"><svg /></div>"))
   )
 ))
