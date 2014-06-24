7a32545f1b99a1983cd797e89a6c541b8ed3f43e
Anonymous@example.com <cpsievert1@gmail.com>
2014-06-23 22:53:41 -0500

    Deprecate runVis in favor of runShiny

--- R/runVis.R
+++ R/runVis.R
@@ -1,49 +1,14 @@
 #' Run shiny/D3 visualization
 #' 
-#' This function is a very small wrapper around \link{shiny::runApp}. Before 
-#' calling this function, make sure you have four objects named 'phi', 
-#' 'term.frequency', 'vocab', and 'topic.proportion' exist in the global 
-#' environment and you also \link{check.inputs}.
+#' This function was deprecated and replaced by \link{runShiny}
 #' 
-#' @param ... Arguments passed to \link{shiny::runApp}.
+#' @param ... Arguments passed to shiny::runApp.
 #' @seealso \link{check.inputs}
-#' @importFrom shiny runApp
 #' @export
-#' @examples
-#' 
-#' # Example using AP documents from 
-#' # http://www.cs.princeton.edu/~blei/lda-c/ap.tgz
-#' data("APdata", package = "LDAvis")
-#' 
-#' # Check the coherence of the input data using the function 'check.inputs()'
-#' z <- check.inputs(K=40, W=10473, phi=APdata$phi, 
-#'                   term.frequency=APdata$term.frequency, vocab=APdata$vocab, 
-#'                   topic.proportion=APdata$topic.proportion)
-#'                  
-#' # Assign the six elements of z as variables in the global environment and
-#' # remove their list versions:
-#' for (i in 1:length(z)) assign(names(z)[i], z[[i]])
-#' 
-#' # Now run the visualization app
-#' runVis()
-#' 
-#' # Example using Newsgroup documents from 
-#' # http://qwone.com/~jason/20Newsgroups/
-#' data("Newsgroupdata", package = "LDAvis")
-#' z <- check.inputs(K=50, W=22524, phi=Newsgroupdata$phi, 
-#'                   term.frequency=Newsgroupdata$term.frequency, 
-#'                   vocab=Newsgroupdata$vocab, 
-#'                   topic.proportion=Newsgroupdata$topic.proportion)
-#' for (i in 1:length(z)) assign(names(z)[i], z[[i]])
-#' 
-#' # Set a seed to ensure k-means produces the same clusters everytime 
-#' # you call runVis():
-#' set.seed(333)
-#' runVis()
 #'
 
 runVis <- function(...) {
-  shinyDir <- system.file("shiny", package = "LDAvis")
-  runApp(appDir = shinyDir, ...)
+  message("runVis is deprecated. Please see ?runShiny or ?createJSON.")
+  return(NULL)
 }
   
