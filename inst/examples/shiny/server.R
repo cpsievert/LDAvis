99e85ec8bd6b2548c4f46c5caae10a3a244e0875
Chris Baker <chriscrewbaker@gmail.com>
2018-04-23 11:21:13 -0400

    update example shiny app to include demo of input$ldavis_term_clicked and input$ldavis_topic_clicked

--- inst/examples/shiny/server.R
+++ inst/examples/shiny/server.R
@@ -5,4 +5,14 @@ shinyServer(function(input, output, session) {
     with(TwentyNewsgroups,
          createJSON(phi, theta, doc.length, vocab, term.frequency,
                     R = input$nTerms))})
+
+  output$termClicked <- renderPrint({
+    if (is.null(input$ldavis_term_clicked)) return()
+    paste("You clicked on term:", input$ldavis_term_clicked)    
+  })
+  output$topicClicked <- renderPrint({
+    if (is.null(input$ldavis_topic_clicked)) return()
+    paste("You clicked on topic:", input$ldavis_topic_clicked)    
+  })
+
 })
--- inst/examples/shiny/ui.R
+++ inst/examples/shiny/ui.R
@@ -2,6 +2,8 @@ library(LDAvis)
 shinyUI(
   fluidPage(
     sliderInput("nTerms", "Number of terms to display", min = 20, max = 40, value = 30),
+    textOutput("termClicked"),
+    textOutput("topicClicked"),
     visOutput('myChart')
   )
 )
