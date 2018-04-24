5331be4f744a67e46dc396b52054da7d836194d5
Carson Sievert <cpsievert1@gmail.com>
2018-04-24 15:14:56 -0500

    update news, tidy up

--- inst/examples/shiny/server.R
+++ inst/examples/shiny/server.R
@@ -4,15 +4,16 @@ shinyServer(function(input, output, session) {
   output$myChart <- renderVis({
     with(TwentyNewsgroups,
          createJSON(phi, theta, doc.length, vocab, term.frequency,
-                    R = input$nTerms))})
+                    R = input$nTerms))
+  })
 
   output$termClicked <- renderPrint({
     if (is.null(input$myChart_term_click)) return()
     paste("You clicked on term:", input$myChart_term_click)    
   })
+  
   output$topicClicked <- renderPrint({
     if (is.null(input$myChart_topic_click)) return()
     paste("You clicked on topic:", input$myChart_topic_click)    
   })
-  
 })
