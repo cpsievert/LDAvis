ceeebfe11b7a682bddc1b8d86728770e8c33d288
Carson Sievert <cpsievert1@gmail.com>
2018-04-25 09:29:21 -0500

    throw a warning instead of error when checking row sums (in case there is a tolerance issue), closes #68

--- R/createJSON.R
+++ R/createJSON.R
@@ -153,8 +153,8 @@ createJSON <- function(phi = matrix(), theta = matrix(), doc.length = integer(),
   phi.test <- all.equal(rowSums(phi), rep(1, K), check.attributes = FALSE)
   theta.test <- all.equal(rowSums(theta), rep(1, dt[1]), 
                           check.attributes = FALSE)
-  if (!isTRUE(phi.test)) stop("Rows of phi don't all sum to 1.")
-  if (!isTRUE(theta.test)) stop("Rows of theta don't all sum to 1.")
+  if (!isTRUE(phi.test)) warning("Rows of phi don't all sum to 1.")
+  if (!isTRUE(theta.test)) warning("Rows of theta don't all sum to 1.")
 
   # compute counts of tokens across K topics (length-K vector):
   # (this determines the areas of the default topic circles when no term is 
